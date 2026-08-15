40e7f601b16d0b4b2b0b7fc5933129f0d54c0152
koti1241 <48479387+koti1241@users.noreply.github.com>
2026-08-15 18:53:21 +0530

    Update main.tf

--- Episode-03/terraform-project/main.tf
+++ Episode-03/terraform-project/main.tf
@@ -29,7 +29,6 @@ provider "google" {
 variable "gcp_project" {
   description = "GCP project ID"
   type        = string
-  default     = "project-b521915a-fd11-4b0a-97f"
 }
 
 variable "gcp_region" {
